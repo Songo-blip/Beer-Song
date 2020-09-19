7c2fc1bb1a578bf5f48791e5dbd418b0be4817c5
Sisipo Songo <ssongo@my365-uwc.ac.za>
2020-09-19 20:48:33 +0200

    Add project files.

---
+++ Beer Song/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BeerSong
+{
+    class Program
+    {
+                static void Main(string[] args)
+        {
+            for (int counter = 99; counter >= 0; counter--)
+            {
+                Console.WriteLine(counter + "bottles of beer on the wall, bottles of beer.");
+                Console.WriteLine((counter) + "Take one down and pass it around, bottles of beer on the wall.");
+            }
+        }
+    }
+}
+
