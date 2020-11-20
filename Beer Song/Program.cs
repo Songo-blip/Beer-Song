9a9d2332e523422fde9c570f59ff66cac62bd62f
Sisipo Songo <ssongo@my365-uwc.ac.za>
2020-11-20 12:28:49 +0200

    update

--- Beer Song/Program.cs
+++ Beer Song/Program.cs
@@ -3,16 +3,46 @@ using System.Collections.Generic;
 
 namespace BeerSong
 {
+
     class Program
     {
-                static void Main(string[] args)
+        public static Main(string [] args)
         {
-            for (int counter = 99; counter >= 0; counter--)
+            string BeerSong();
+        
+            string lyrics = string.Empty;
+
+            int startingpoint = 99;
+
+            for (int index = startingpoint; index > 0; index--)
             {
-                Console.WriteLine(counter + "bottles of beer on the wall, bottles of beer.");
-                Console.WriteLine((counter) + "Take one down and pass it around, bottles of beer on the wall.");
+                string line = string.Empty;
+
+                if (index != 1)
+                {
+                    if (index == 2)
+                    {
+                        line = $"{index} bottles of beer on the wall,{index} bottles of beer." + "\n";
+                        line = $"Take one down and pass it around,{index - 1} bottles of beer on the wall." + "\n";
+                    }
+                    else
+                    {
+                        line = $"{index} bottles of beer on the wall,{index} bottles of beer." + "\n";
+                        line = $"Take one down and pass it around,{index - 1} bottles of beer on the wall." + "\n";
+                    }
+                }
+                else
+                {
+                    line = $"{index} bottles of beer on the wall,{index} bottles of beer." + "\n";
+                    line = $"Take one down and pass it around,{index - 1} bottles of beer on the wall." + "\n";
+
+                }
+                lyrics += line + "\n";
             }
+            return lyrics;
         }
     }
 }
+   
+ 
 
